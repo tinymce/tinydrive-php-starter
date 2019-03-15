90da9eb2d7fe17a7cf0e0cceb2f5dbadb85b6288
Fredrik Danielsson <fredrik@lostkeys.se>
2019-03-15 12:03:04 +0100

    Linting, removed app.css

--- editor.php
+++ editor.php
@@ -11,21 +11,21 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Editor</title>
-    <link rel="stylesheet" href="assets/css/app.css">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-  </head>
+
+<head>
+  <meta charset="utf-8">
+  <title>Editor</title>
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+
 <body>
-<div class="container mt-3 mb-3">
-  <h1 class="h3 mb-4">Welcome <?php echo $user["fullname"]; ?> <a class="logout" href="logout.php">Logout</a></h1>
-  <p>TinyMCE editor instance setup with Tiny Drive integated into the link, image and media dialogs and with a separate direct insertfile button.</p>
-  <textarea></textarea>
-</div>
+  <div class="container mt-3 mb-3">
+    <h1 class="h3 mb-4">Welcome <?php echo $user["fullname"]; ?> <a class="logout" href="logout.php">Logout</a></h1>
+    <p>TinyMCE editor instance setup with Tiny Drive integated into the link, image and media dialogs and with a separate direct insertfile button.</p>
+    <textarea></textarea>
+  </div>
 
-<script src="https://cloud.tinymce.com/5/tinymce.min.js?apiKey=<?php echo $config["apiKey"]; ?>"></script>
-<script src="assets/js/init.js"></script>
-
+  <script src="https://cloud.tinymce.com/5/tinymce.min.js?apiKey=<?php echo $config["apiKey"]; ?>"></script>
+  <script src="assets/js/init.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -25,38 +25,37 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Login</title>
-    <link rel="stylesheet" href="assets/css/app.css">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-  </head>
+
+<head>
+  <meta charset="utf-8">
+  <title>Login</title>
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+
 <body>
+  <div class="container vh-100 d-flex justify-content-center align-items-center">
+    <form method="post" action="index.php">
+      <?php if ($error !== "") { ?>
+        <div class="alert alert-danger" role="alert"><?php echo $error; ?></div>
+      <?php } ?>
 
-<div class="container vh-100 d-flex justify-content-center align-items-center">
-  <form method="post" action="index.php">
-    <?php if ($error !== "") { ?>
-      <div class="alert alert-danger" role="alert"><?php echo $error; ?></div>
-    <?php } ?>
+      <h1 class="h3 mb-3">Login</h1>
+      <p>Login with the user/passwords available in the <code>config.php</code> file</p>
 
-    <h1 class="h3 mb-3">Login</h1>
-    <p>Login with the user/passwords available in the <code>config.php</code> file</p>
+      <div class="form-group">
+        <label for="username" class="font-weight-bold">User</label>
+        <input type="text" class="form-control" id="username" name="username" value="johndoe">
+      </div>
 
-    <div class="form-group">
-      <label for="username" class="font-weight-bold">User</label>
-      <input type="text" class="form-control" id="username" name="username" value="johndoe">
-    </div>
+      <div class="form-group">
+        <label for="password" class="font-weight-bold">Password</label>
+        <input type="password" class="form-control" id="password" name="password" value="password">
+      </div>
 
-    <div class="form-group">
-      <label for="password" class="font-weight-bold">Password</label>
-      <input type="password" class="form-control" id="password" name="password" value="password">
-    </div>
-
-    <div class="form-group">
-      <input type="submit" class="btn btn-primary" value="Login">
-    </div>
-  </form>
-</div>
-
+      <div class="form-group">
+        <input type="submit" class="btn btn-primary" value="Login">
+      </div>
+    </form>
+  </div>
 </body>
 </html>
