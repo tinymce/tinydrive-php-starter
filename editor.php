1e6a13f08d15c965d5e807a2feeedd389f102f1c
Fredrik Danielsson <fredrik@lostkeys.se>
2019-03-15 13:45:53 +0100

    Use Bootstrap CDN instead of local files

--- editor.php
+++ editor.php
@@ -15,7 +15,7 @@
 <head>
   <meta charset="utf-8">
   <title>Editor</title>
-  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
