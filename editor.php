555266b31a5b5a2cfee1b5b54a4c61939da04fcc
Fredrik Danielsson <fredrik@lostkeys.se>
2019-03-15 12:00:43 +0100

    Bootstrap styling

--- editor.php
+++ editor.php
@@ -15,12 +15,17 @@
     <meta charset="utf-8">
     <title>Editor</title>
     <link rel="stylesheet" href="assets/css/app.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
   </head>
 <body>
-  <h1>Welcome <?php echo $user["fullname"]; ?> <a class="logout" href="logout.php">Logout</a></h1>
+<div class="container mt-3 mb-3">
+  <h1 class="h3 mb-4">Welcome <?php echo $user["fullname"]; ?> <a class="logout" href="logout.php">Logout</a></h1>
   <p>TinyMCE editor instance setup with Tiny Drive integated into the link, image and media dialogs and with a separate direct insertfile button.</p>
   <textarea></textarea>
-  <script src="https://cloud.tinymce.com/5/tinymce.min.js?apiKey=<?php echo $config["apiKey"]; ?>"></script>
-  <script src="assets/js/init.js"></script>
+</div>
+
+<script src="https://cloud.tinymce.com/5/tinymce.min.js?apiKey=<?php echo $config["apiKey"]; ?>"></script>
+<script src="assets/js/init.js"></script>
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -29,26 +29,34 @@
     <meta charset="utf-8">
     <title>Login</title>
     <link rel="stylesheet" href="assets/css/app.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
   </head>
 <body>
-<form method="post" action="index.php">
-  <?php if ($error !== "") { ?>
-    <div><?php echo $error; ?></div>
-  <?php } ?>
 
-  <p>Login with the user/passwords available in the config.php file</p>
+<div class="container vh-100 d-flex justify-content-center align-items-center">
+  <form method="post" action="index.php">
+    <?php if ($error !== "") { ?>
+      <div class="alert alert-danger" role="alert"><?php echo $error; ?></div>
+    <?php } ?>
 
-  <div>
-    <label for="username">User:</label>
-    <input type="text" id="username" name="username" value="johndoe">
-  </div>
+    <h1 class="h3 mb-3">Login</h1>
+    <p>Login with the user/passwords available in the <code>config.php</code> file</p>
 
-  <div>
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password" value="password">
-  </div>
+    <div class="form-group">
+      <label for="username" class="font-weight-bold">User</label>
+      <input type="text" class="form-control" id="username" name="username" value="johndoe">
+    </div>
 
-  <input type="submit" value="login">
-</form>
+    <div class="form-group">
+      <label for="password" class="font-weight-bold">Password</label>
+      <input type="password" class="form-control" id="password" name="password" value="password">
+    </div>
+
+    <div class="form-group">
+      <input type="submit" class="btn btn-primary" value="Login">
+    </div>
+  </form>
+</div>
+
 </body>
 </html>
